e6f723dffa68dbf85a83ccf74a3f04749287874c
Harsh Kumar Saini <150950663+Culebrin19@users.noreply.github.com>
2024-12-30 15:22:01 +0100

    sol

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,12 @@
+/* eslint-env browser */
+
+// Función para configurar el canvas
 function setup() {
   // Crear un canvas de 600x400 píxeles y asignarlo al contenedor con ID "sketch-holder"
   createCanvas(600, 400).parent("#sketch-holder");
 }
 
+// Función para dibujar el contenido en el canvas
 function draw() {
   // Fondo azul cielo
   background(135, 206, 235);
